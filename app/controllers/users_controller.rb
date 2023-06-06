5257478a7f225cba695a6aeccec4b3a4a0717678
Nikita Butnar <nick.butnar@jetrockets.com>
2023-06-07 02:45:28 +0400

    user registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def new
+    user = User.new
+    render locals: { user: user }
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to root_path
+      flash[:success] = "Welcome to the app, #{user.name}"
+    else
+      flash[:error] = 'Abort registration. Please, try again'
+      render :new, locals: { user: user }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'products', to: 'products#index'
   get 'product/:id', to: 'products#show', as: 'product'
 
+  resources :users, only: %i[new create]
+
   namespace 'admin' do
     resources :products, only: %i[index new create edit update destroy]
     # patch 'products/:id', to: 'products#update', as: 'update_product'

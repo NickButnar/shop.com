1273e7dd4d4cb94a8d135d36b65de94f3c0a0b87
Nikita Butnar <nick.butnar@jetrockets.com>
2023-06-08 14:52:41 +0400

    user log in check

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  before_action :require_no_authentication, only: %i[new create]
+  before_action :require_authentication, only: %i[destroy]
+  def new
+  end
+
+  def create
+    user = User.find_by email: params[:email]
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+      flash[:success] = "Welcome to the app, #{user.name}"
+    else
+      flash[:error] = 'Incorrect email and/or password'
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    flash[:success] = 'See you later'
+    redirect_to root_url
+  end
+end

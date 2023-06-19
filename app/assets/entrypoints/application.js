4845c4c81507b719a9a371dd637eeeba32c05960
Nikita Butnar <nick.butnar@jetrockets.com>
2023-06-19 11:59:41 +0400

    create folders. install turbo rails. use turbo to edit folder

--- app/assets/entrypoints/application.js
+++ app/assets/entrypoints/application.js
@@ -1,11 +1,13 @@
 // Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
+
+
+import * as Turbo from "@hotwired/turbo"
+
+Turbo.start()
+
 // import ActiveStorage from '@rails/activestorage'
 // ActiveStorage.start()
-//
+
 // // Import all channels.
 // const channels = import.meta.globEager('./**/*_channel.js')
 
